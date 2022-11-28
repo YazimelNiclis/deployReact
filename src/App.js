d81ebdca29c27cefb3bdbeff1f1107a50a4810ef
yazimel.niclis <yazimelniclis1995@gmail.com>
2022-11-28 09:03:22 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Yazi github
+          Learn React with Yazi
         </a>
       </header>
     </div>
