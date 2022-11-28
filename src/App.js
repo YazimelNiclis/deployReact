a79e3362a21fa251e65c5a1653bc04d31dbe27cb
yazimel.niclis <yazimelniclis1995@gmail.com>
2022-11-28 08:37:13 -0300

    Cambios en App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/YazimelNiclis"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Yazi
         </a>
       </header>
     </div>
